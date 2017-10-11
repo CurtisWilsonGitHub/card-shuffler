eced6cc933f006694fb77065f97de0323d841a3a
Curtis Wilson <curtiswemail@gmail.com>
2017-10-10 22:09:08 -0600

    added deck object

--- app.js
+++ app.js
@@ -1,43 +1,72 @@
-function transposeTwoStrings(arr){
-//your code here
+function cardShuffle(){
 
-//function to perform the transposing
-var transposer = function(arr){
-  var stringSequence = '';
+/* object cards represent a deck of standard playing cards. Suit is lower case
+  (i.e. 1s represents '1 of spade'). Jokers are not included. */
 
-  for(var i = 0; i < wordCount; i++){
-      debugger;
-      if(arr[0].charAt(i) != ''){
-        stringSequence = stringSequence.concat(arr[0].charAt(i));
-      }
-      else{ stringSequence = stringSequence + " ";}
-      stringSequence = stringSequence +" ";
-      if(arr[1].charAt(i) != ''){
-        stringSequence = stringSequence.concat(arr[1].charAt(i));
-      }
-      else{ stringSequence = stringSequence + " ";}
-
-      if( i === wordCount -1){break}
-
-      stringSequence = stringSequence + "\n";
+  var cards = {
+    1: "1s",
+    2: "2s",
+    3: "3s",
+    4: "4s",
+    5: "5s",
+    6: "6s",
+    7: "7s",
+    8: "8s",
+    9: "9s",
+    10: "Js",
+    11: "Qs",
+    12: "Ks",
+    13: "As",
+    14: "1c",
+    15: "2c",
+    16: "3c",
+    17: "4c",
+    18: "5c",
+    19: "6c",
+    20: "7c",
+    21: "8c",
+    22: "9c",
+    23: "Jc",
+    24: "Qc",
+    25: "Kc",
+    26: "Ac",
+    27: "1h",
+    28: "2h",
+    29: "3h",
+    30: "4h",
+    31: "5h",
+    32: "6h",
+    33: "7h",
+    34: "8h",
+    35: "9h",
+    36: "Jh",
+    37: "Qh",
+    38: "Kh",
+    39: "Ah",
+    40: "1d",
+    41: "2d",
+    42: "3d",
+    43: "4d",
+    44: "5d",
+    45: "6d",
+    46: "7d",
+    47: "8d",
+    48: "9d",
+    49: "Jd",
+    50: "Qd",
+    51: "Kd",
+    52: "Ad"
   }
-  return(stringSequence);
-}
 
-// test to see if strings are blank
-if(arr[0] == '' && arr[1] == ''){return('')};
+/* function that'll create an array of consisting of numbers
+1-52 randomized. */
 
-// test to see which word is longer
-
-var wordCount = 0;
-
-if(arr[0].length > arr[1]. length){ wordCount = arr[0].length}else{wordCount = arr[1].length}
-
-console.log(transposer(arr));
-
+var deckMaker = function(){
+  
 }
 
 
+  console.log(cards);
+}
 
-
-transposeTwoStrings(['cat','']);
+cardShuffle();
